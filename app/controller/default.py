74542eff59e90154745b71a4beb11d0cad3e5eb5
Isabelly Dias <isabellydias29@gmail.com>
2021-05-20 20:17:41 -0300

    Finalizando o backend

--- app/controller/default.py
+++ app/controller/default.py
@@ -2,13 +2,23 @@ from flask import render_template, request, redirect, url_for, make_response
 from flask_login import login_user, logout_user
 from app import app, db
 from app.model.tables import User, Servico, Material, MaterialServico
-from json import loads, dumps
+from ast import literal_eval
 from sqlalchemy import func
 
 
 @app.route('/')
 def home():
     return render_template('home.html')
+
+
+@app.route('/cadastromateriais')
+def cadastrar_materiais():
+    lista_material = request.cookies.get('materiais')
+    if lista_material:
+        literal_eval(lista_material)
+    else:
+        lista_material = {}
+    return render_template('cadastro_materiais.html', valor_total=0, lista_material=lista_material)
 
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -50,16 +60,25 @@ def logout():
 
 @app.route('/servico', methods=['GET', 'POST'])
 def servico():
-    if request.method == 'POST':
-        descricao = request.form['descricao']
-        valor_mao_de_obra =  request.form['valor_mao_de_obra']
-        servico = Servico(descricao, valor_mao_de_obra, 0)
-        lista_material = loads(request.cookies.get('materiais'))
-        servico = calcularValorTotal(servico, lista_material)
-        db.session.add(servico)
-        id_servico = Servico.query(func.max(Servico.id))
+    servico_dic = literal_eval(request.cookies.get('servico'))
+    servico = Servico(servico_dic['descricao'], servico_dic['valor_mao_de_obra'], servico_dic['valor_total'])
+    lista_material = literal_eval(request.cookies.get('materiais'))
+    db.session.add(servico)
+    db.session.commit()
+    id_servico = db.session.query(func.max(Servico.id))
+    for material in lista_material:
+        material = Material(lista_material[material]['preco'], lista_material[material]['descricao'],
+                            lista_material[material]['tipo'])
+        db.session.add(material)
         db.session.commit()
-
+        id_material = db.session.query(func.max(Material.id))
+        material_servico = MaterialServico(id_material, id_servico)
+        db.session.add(material_servico)
+        db.session.commit()
+    resp = make_response(redirect('/cadastromateriais'))
+    resp.set_cookie('servico', '', expires=0)
+    resp.set_cookie('materiais', '', expires=0)
+    return resp
 
 
 @app.route('/adicionarmaterial', methods=['POST'])
@@ -68,28 +87,36 @@ def adicionar_material():
     descricao = request.form['descricao']
     tipo = request.form['tipo']
 
-    dic = loads(request.cookies.get('materiais'))
+    dic = request.cookies.get('materiais')
     if dic:
-        key = 'Material' + str(len(dic))
+        dic = literal_eval(dic)
+        key = 'Material' + str(len(dic) + 1)
         dic[key] = {"preco": preco, "descricao": descricao, "tipo": tipo}
-        resp = make_response(render_template('cadastro_materiais.html'))
-        resp.set_cookie('materiais', dic)
+        resp = make_response(render_template('cadastro_materiais.html', lista_material=dic, valor_total=0))
+        resp.set_cookie('materiais', str(dic))
 
     else:
-        key = 'Material' + str(len(dic)+1)
+        key = 'Material' + str(1)
+        dic = {}
         dic[key] = {"preco": preco, "descricao": descricao, "tipo": tipo}
-        resp = make_response(render_template('cadastro_materiais.html'))
-        resp.set_cookie('materiais', dic)
+        resp = make_response(render_template('cadastro_materiais.html', lista_material=dic, valor_total=0))
+        resp.set_cookie('materiais', str(dic))
 
     return resp
 
-@app.route('/calcularvalortotal/<servico>/<lista_material>', methods=['GET'])
-def calcularValorTotal(servico, lista_material):
-    valor_total_material = 0
-    for material in lista_material:
-        valor_total_material += lista_material[material]['preco']
 
-    servico.valor_total = valor_total_material
-    servico.valor_total += servico.valor_mao_de_obra
-    resp = make_response(render_template('cadastro_materiais.html'))
-    resp.set_cookie('materiais', servico)
+@app.route('/calcularvalortotal', methods=['POST'])
+def calcular_valor_total():
+    if request.method == 'POST':
+        descricao = request.form['descricao']
+        valor_mao_de_obra = request.form['valormaodeobra']
+        valor_total = 0
+        lista_material = literal_eval(request.cookies.get('materiais'))
+        for material in lista_material:
+            valor_total += int(lista_material[material]['preco'])
+
+        valor_total += int(valor_mao_de_obra)
+        servico = {"descricao": descricao, "valor_mao_de_obra": valor_mao_de_obra, "valor_total": valor_total}
+        resp = make_response(render_template('cadastro_materiais.html', valor_total=valor_total, lista_material=lista_material))
+        resp.set_cookie('servico', str(servico))
+        return resp
